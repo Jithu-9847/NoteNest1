55514d7a5a71fd1b0254b83780cafc62314d0d42
Jithu-9847 <131839320+Jithu-9847@users.noreply.github.com>
2025-01-09 23:00:27 +0530

    New Group Chat feature Added

--- src/bottom_page/BottomPage.js
+++ src/bottom_page/BottomPage.js
@@ -6,7 +6,7 @@ function BottomPage() {
             <footer className="container mx-auto px-4">
                 <div className="flex flex-col items-center justify-center">
                     <p className="text-center text-sm">
-                        &copy; 2024 Note Nest. All rights reserved. Made with <span className=' text-3xl'>❤️</span> NOTE NEST.
+                        &copy; 2025 Note Nest. All rights reserved. Made with <span className=' text-3xl'>❤️</span> NOTE NEST.
                     </p>
                     <ul className="flex space-x-6 mt-4">
                         <li><a href="/PrivacyPolicy" className="text-gray-400 hover:text-gray-200 transition duration-200">Privacy Policy</a></li>
